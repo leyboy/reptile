3fb52d543ef4a10f3c70171a5733dfb1ddea07c7
leyboy <2036185346@qq.com>
2017-09-12 18:45:12 +0800

    reptile bean book model

---
+++ src/main/java/com/reptile/bean/Book.java
@@ -0,0 +1,54 @@
+package com.reptile.bean;
+
+/**
+ * @author LiuEnYuan
+ * @version 1.1 封装了获取京东书的实体类
+ **/
+public class Book {
+
+	private Integer bookId;
+	private String bookName;
+	private Double bookPrice;
+
+	public Book() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Book(Integer bookId, String bookName, Double bookPrice) {
+		super();
+		this.bookId = bookId;
+		this.bookName = bookName;
+		this.bookPrice = bookPrice;
+	}
+
+	public Integer getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(Integer bookId) {
+		this.bookId = bookId;
+	}
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public Double getBookPrice() {
+		return bookPrice;
+	}
+
+	public void setBookPrice(Double bookPrice) {
+		this.bookPrice = bookPrice;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [bookId=" + bookId + ", bookName=" + bookName + ", bookPrice=" + bookPrice + "]";
+	}
+
+}
